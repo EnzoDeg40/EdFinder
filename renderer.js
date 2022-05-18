31dfa8bc1539c170140e0026d2d172ef994d95e6
Enzo Degraeve <enzodeg40@gmail.com>
2022-05-18 17:55:34 +0200

    Menu list

--- renderer.js
+++ renderer.js
@@ -25,7 +25,7 @@ try {
     console.error(err)
 }
 
-ls('./node_modules/');
+ls('./');
 
 onmousemove = function (e) {
     let margincursor = 20;
@@ -33,3 +33,29 @@ onmousemove = function (e) {
     usercusor.style.top = (e.clientY + margincursor) + "px";
     usercusor.style.left = (e.clientX + margincursor) + "px";
 }
+
+var menu = document.getElementById('menu');
+
+menu.addEventListener('click', event => {
+    // Check if it’s the list element, we want the clicks only from them
+    //if (event.target.classList.contains('list--item')) {
+        console.log('The item ' + event.target.innerHTML + ' was just clicked')
+    //}
+
+    switch (event.target.innerHTML) {
+        case 'Enzo': ls('C:\\Users\\enzod'); break;
+        case 'Racine /': ls('C:\\'); break;
+        case 'Disque C': ls('C:\\'); break;
+        case 'Disque D': ls('D:\\'); break;
+        case 'Disque E': ls('E:\\'); break;
+        case 'Disque F': ls('F:\\'); break;
+
+
+        default:
+            console.log('Nothing was just clicked')
+    }
+})
+
+
+
+//ls('C:\\Users\\enzod')
